883e4590ad1b8f14508fd7710c19d7f8abddc5c7
Athrysd <221656567+Athrysd@users.noreply.github.com>
2025-12-29 06:55:28 +0700

    modified:   package-lock.json modified:   package.json modified:   src/App.jsx modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Rute from '../src/Routing/Routing.jsx'
 import ScrollToTop from './components/ScrollToUp/SCT.jsx'
+import { Analytics } from '@vercel/analytics/react';
+
+
 
 function App() {
 
   return (
     <>
       <ScrollToTop />
+      <Analytics />
       <Rute />
     </>
   )
